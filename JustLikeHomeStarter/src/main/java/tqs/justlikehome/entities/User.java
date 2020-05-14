2c0818f119302d8e93c7011f668906e6ea12c50f
joaonpsilva <joaonuno.dragao@gmail.com>
2020-05-14 19:02:16 +0100

    Long to long bug

--- JustLikeHomeStarter/src/main/java/tqs/justlikehome/entities/User.java
+++ JustLikeHomeStarter/src/main/java/tqs/justlikehome/entities/User.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String username;
     private String firstName;
     private String lastName;
