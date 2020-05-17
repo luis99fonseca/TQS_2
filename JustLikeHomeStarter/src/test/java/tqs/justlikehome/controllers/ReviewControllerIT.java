ccbf1174031997005bc9c23fbe1c57e7b42ea300
joaonpsilva <joaonuno.dragao@gmail.com>
2020-05-17 20:32:47 +0100

    try catch instead of if

---
+++ JustLikeHomeStarter/src/test/java/tqs/justlikehome/controllers/ReviewControllerIT.java
@@ -0,0 +1,101 @@
+package tqs.justlikehome.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+
+import tqs.justlikehome.JustlikehomeApplication;
+import tqs.justlikehome.dtos.HouseReviewDTO;
+import tqs.justlikehome.entities.House;
+import tqs.justlikehome.entities.Rent;
+import tqs.justlikehome.entities.User;
+import tqs.justlikehome.repositories.HouseRepository;
+import tqs.justlikehome.repositories.UserRepository;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = JustlikehomeApplication.class)
+@AutoConfigureMockMvc
+public class ReviewControllerIT {
+    @LocalServerPort
+    int randomServerPort;
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Autowired
+    private HouseRepository houseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    private User owner;
+    private House house;
+
+
+    @BeforeEach
+    private void setup(){
+        userRepository.deleteAll();
+        houseRepository.deleteAll();
+        
+        user = new User("Fonsequini","Luis","Fonseca",new GregorianCalendar(1999, Calendar.JULY,20));
+        owner = new User("JaoSiuba", "Joao", "Silva", new GregorianCalendar(1999, Calendar.OCTOBER,25));
+        house = new House(
+                "Aveiro",
+                "Incredible House near Ria de Aveiro",
+                3.0,
+                50.0,
+                2,
+                5
+        );
+        owner.addHouse(house);
+        house.setOwner(owner);
+
+        Date start = Date.from(new GregorianCalendar(2019, Calendar.JULY,20).toZonedDateTime().toInstant());
+        Date end = Date.from(new GregorianCalendar(2019, Calendar.JULY,22).toZonedDateTime().toInstant());
+        Rent rent = new Rent(house,user,start,end);
+        house.addRent(rent);
+        user.addPurchasedRent(rent);
+
+        owner = userRepository.save(owner);
+        user = userRepository.save(user);
+    }
+    
+    @Test
+    public void addHouseReview() throws Exception {
+        HouseReviewDTO hrdto = new HouseReviewDTO(user.getId(), house.getId(), 3.0, "good");
+        mvc.perform(post("/newHouseReview").contentType(MediaType.APPLICATION_JSON).content(objectToJson(hrdto)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.user.username",is("Fonsequini")))
+            .andExpect(jsonPath("$.house.description",is("Incredible House near Ria de Aveiro")))
+            .andExpect(jsonPath("$.house.id",is(0)))
+            .andExpect(jsonPath("$.user.id",is(0)));
+
+    }
+
+    private String objectToJson(Object obj){
+        try{
+            return new ObjectMapper().writeValueAsString(obj);
+        }catch (JsonProcessingException e){
+            throw new RuntimeException();
+        }
+    }
+}
