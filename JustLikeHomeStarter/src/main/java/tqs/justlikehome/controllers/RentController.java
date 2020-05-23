63267cd6412e88a5674fd5337c88f79e146e4ce6
MotaMiguel <miguel.mota@ua.pt>
2020-05-23 14:10:23 +0100

    added valid annotation

--- JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/RentController.java
+++ JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/RentController.java
@@ -6,6 +6,7 @@ import tqs.justlikehome.dtos.RentDTO;
 import tqs.justlikehome.entities.Rent;
 import tqs.justlikehome.services.RentService;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -18,31 +19,31 @@ public class RentController {
 
     @PostMapping(value="/askToRent")
     @ResponseBody
-    public Rent askToRent(@RequestBody RentDTO rentDTO){
+    public Rent askToRent(@Valid  @RequestBody RentDTO rentDTO){
         return rentService.askToRent(rentDTO);
     }
 
     @PutMapping(value="/acceptRent")
     @ResponseBody
-    public Rent acceptRent(@RequestBody Map<String,Long> rentID){
+    public Rent acceptRent(@Valid @RequestBody Map<String,Long> rentID){
         return rentService.acceptRent(rentID);
     }
 
     @PutMapping(value="/denyRent")
     @ResponseBody
-    public Rent denyRent(@RequestBody Map<String, Long> rentID){
+    public Rent denyRent(@Valid @RequestBody Map<String, Long> rentID){
         return rentService.denyRent(rentID);
     }
 
     @GetMapping(value="/pendingRents/user={userID}")
     @ResponseBody
-    public List<Rent> pendingRents(@PathVariable long userID){
+    public List<Rent> pendingRents(@Valid @PathVariable long userID){
         return rentService.pendingRents(userID);
     }
 
     @GetMapping(value="/onGoingRents/user={userID}")
     @ResponseBody
-    public List<Rent> onGoingRents(@PathVariable long userID){
+    public List<Rent> onGoingRents(@Valid @PathVariable long userID){
         return rentService.onGoingRents(userID);
     }
 
--- JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/UserController.java
+++ JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/UserController.java
@@ -7,6 +7,8 @@ import tqs.justlikehome.dtos.UserDTO;
 import tqs.justlikehome.entities.House;
 import tqs.justlikehome.entities.User;
 import tqs.justlikehome.services.UserService;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,18 +20,18 @@ public class UserController {
 
     @PostMapping(value = "/newHouse")
     @ResponseBody
-    public House addHouseToUser(@RequestBody HouseDTO house) {
+    public House addHouseToUser(@Valid  @RequestBody HouseDTO house) {
         return userService.addHouseToUser(house);
     }
 
     @PostMapping(value = "/createUser")
     @ResponseBody
-    public User createUser(@RequestBody UserDTO userDTO) {
+    public User createUser(@Valid @RequestBody UserDTO userDTO) {
         return userService.createUser(userDTO);
     }
 
     @GetMapping(value="/userHouses/user={userId}")
-    public List<House> getUserHouses(@PathVariable long userId){
+    public List<House> getUserHouses(@Valid @PathVariable long userId){
         return userService.getUserHouses(userId);
     }
 }
