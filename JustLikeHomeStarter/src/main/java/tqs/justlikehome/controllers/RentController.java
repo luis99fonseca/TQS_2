14d7f00569d8970b358aa9e8f7f2cb1fcb3ec896
MotaMiguel <miguel.mota@ua.pt>
2020-05-28 01:22:24 +0100

    Added description to documentation api

--- JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/RentController.java
+++ JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/RentController.java
@@ -1,5 +1,6 @@
 package tqs.justlikehome.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tqs.justlikehome.dtos.RentDTO;
@@ -19,30 +20,35 @@ public class RentController {
 
     @PostMapping(value="/askToRent")
     @ResponseBody
+    @ApiOperation(value="Ask to rent a certain house")
     public Rent askToRent(@Valid  @RequestBody RentDTO rentDTO){
         return rentService.askToRent(rentDTO);
     }
 
     @PutMapping(value="/acceptRent")
     @ResponseBody
+    @ApiOperation(value="Owner accept a user rent request on a given house")
     public Rent acceptRent(@Valid @RequestBody Map<String,Long> rentID){
         return rentService.acceptRent(rentID);
     }
 
     @PutMapping(value="/denyRent")
     @ResponseBody
+    @ApiOperation(value="Owner deny a user rent request on a given house")
     public Rent denyRent(@Valid @RequestBody Map<String, Long> rentID){
         return rentService.denyRent(rentID);
     }
 
     @GetMapping(value="/pendingRents/user={userID}")
     @ResponseBody
+    @ApiOperation(value="Return all pending rents on all houses of a given user")
     public List<Rent> pendingRents(@Valid @PathVariable long userID){
         return rentService.pendingRents(userID);
     }
 
     @GetMapping(value="/onGoingRents/user={userID}")
     @ResponseBody
+    @ApiOperation(value="Return all on going rents on all houses of a given user")
     public List<Rent> onGoingRents(@Valid @PathVariable long userID){
         return rentService.onGoingRents(userID);
     }
--- JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/UserController.java
+++ JustLikeHomeStarter/src/main/java/tqs/justlikehome/controllers/UserController.java
@@ -1,5 +1,6 @@
 package tqs.justlikehome.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tqs.justlikehome.dtos.HouseDTO;
@@ -22,30 +23,36 @@ public class UserController {
 
     @PostMapping(value = "/newHouse")
     @ResponseBody
+    @ApiOperation(value="Create and add a house to a existing user")
     public House addHouseToUser(@Valid  @RequestBody HouseDTO house) {
         return userService.addHouseToUser(house);
     }
 
     @PostMapping(value = "/createUser")
     @ResponseBody
+    @ApiOperation(value="Create a new user")
     public User createUser(@Valid @RequestBody UserDTO userDTO) {
         return userService.createUser(userDTO);
     }
 
     @GetMapping(value="/userHouses/user={userId}")
+    @ApiOperation(value="Return all user houses")
     public List<House> getUserHouses(@Valid @PathVariable long userId){
         return userService.getUserHouses(userId);
     }
 
     @GetMapping(value="/userInfo/user={userId}")
+    @ApiOperation(value="Returns all user information")
     public UserInfoDTO getUserInfo(@Valid @PathVariable long userId){
         return userService.getUserInfo(userId);
     }
 
     @GetMapping(value = "/getAll")
+    @ApiOperation(value="Returns all users")
     public List<User> getAll() { return userService.getAll();}
 
     @GetMapping(value="/login")
+    @ApiOperation(value="User login")
     public Map<String,Long> login(@Valid @RequestHeader("username") String username,@Valid @RequestHeader("password") String password){
         return userService.login(username,password);
     }
