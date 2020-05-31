f42bb8625b64dd4f49b978b2f00e3ba1c6e3ba9d
MotaMiguel <miguel.mota@ua.pt>
2020-05-31 18:27:58 +0100

    Forgot to delete set property

--- JustLikeHomeStarter/src/test/java/tqs/justlikehome/functional/WebPlatformTesting.java
+++ JustLikeHomeStarter/src/test/java/tqs/justlikehome/functional/WebPlatformTesting.java
@@ -52,7 +52,7 @@ public class WebPlatformTesting {
 
     @BeforeEach
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        //System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--headless","--disable-gpu");
